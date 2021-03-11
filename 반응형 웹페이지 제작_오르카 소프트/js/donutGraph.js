37b56dc288737341791400e119e16267295178f7
NYchicken <mirro97@naver.com>
2021-03-11 18:04:06 +0900

    slider 추가, donut graph 추가 완

---
+++ 반응형 웹페이지 제작_오르카 소프트/js/donutGraph.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  const myChart = $("#first-graph").donutty({
+    color: "#e0444c",
+    min: 0,
+    max: 100,
+    value: 90,
+    thickness: 5,
+  });
+});
+$(document).ready(function () {
+  const myChart = $("#second-graph").donutty({
+    color: "#e0444c",
+    min: 0,
+    max: 100,
+    value: 99,
+    thickness: 5,
+  });
+});
+$(document).ready(function () {
+  const myChart = $("#third-graph").donutty({
+    color: "#e0444c",
+    min: 0,
+    max: 100,
+    value: 100,
+    thickness: 5,
+  });
+});
