dd45f1936d9445c3aedab787d20b0fdf70018375
NYchicken <mirro97@naver.com>
2021-03-24 17:02:44 +0900

    공길이 - 로그인 오류 출력 이벤트 완료, main 페이지 제작 중

--- 공길이 웹페이지.ver/app.js
+++ 공길이 웹페이지.ver/app.js
@@ -1,92 +1,106 @@
-// const express = require("express");
-// const app = express();
-// const httpPort = 80;
-// app.listen(httpPort);
+const express = require("express");
+const app = express();
+const httpPort = 80;
+app.listen(httpPort);
 
-// const fs = require("fs");
+const fs = require("fs");
 
-// app.use(express.urlencoded());
-// app.use("/", express.static(__dirname + "/public"));
-// app.use("/component", express.static(__dirname + "/views/component"));
+app.use(express.urlencoded());
+app.use("/", express.static(__dirname + "/public"));
+app.use("/component", express.static(__dirname + "/views/component"));
 
-// app.get("/", (req, res, next) => {
-//   fs.readFile(
-//     __dirname + "/views/content/mainPage.html",
-//     "utf8",
-//     function (err, data) {
-//       res.end(data);
-//     }
-//   );
-// });
-
-// app.get("/main", (req, res, next) => {
-//   fs.readFile(
-//     __dirname + "/views/content/mainPage.html",
-//     "utf8",
-//     function (err, data) {
-//       res.end(data);
-//     }
-//   );
-// });
-
-// app.get("/logIn", (req, res, next) => {
-//   fs.readFile(
-//     __dirname + "/views/content/loginPage.html",
-//     "utf8",
-//     function (err, data) {
-//       res.end(data);
-//     }
-//   );
-// });
-
-// app.get("/logInSuccess", (req, res, next) => {
-//   fs.readFile(
-//     __dirname + "/views/content/loginPage.html",
-//     "utf8",
-//     function (err, data) {
-//       res.end(data);
-//     }
-//   );
-// });
-
-// app.get("/logInSuccess", (req, res, next) => {
-//   fs.readFile(
-//     __dirname + "/views/content/signupPage.html",
-//     "utf8",
-//     function (err, data) {
-//       res.end(data);
-//     }
-//   );
-// });
-
-// app.get("/logInSuccess", (req, res, next) => {
-//   fs.readFile(
-//     __dirname + "/views/content/enterAccount.html",
-//     "utf8",
-//     function (err, data) {
-//       res.end(data);
-//     }
-//   );
-// });
-
-// app.get("/logInSuccess", (req, res, next) => {
-//   fs.readFile(
-//     __dirname + "/views/content/signupComplete.html",
-//     "utf8",
-//     function (err, data) {
-//       res.end(data);
-//     }
-//   );
-// });
-
-var express = require("express");
-var app = express();
-
-app.get("/", function (req, res) {
-  res.send("hello world!");
+app.get("/", (req, res, next) => {
+  fs.readFile(
+    __dirname + "/views/content/mainPage.html",
+    "utf8",
+    function (err, data) {
+      res.end(data);
+    }
+  );
 });
 
-var port = 8080;
-app.listen(port, function () {
-  console.log("server on! http:localhost:" + port);
+app.get("/main", (req, res, next) => {
+  fs.readFile(
+    __dirname + "/views/content/mainPage.html",
+    "utf8",
+    function (err, data) {
+      res.end(data);
+    }
+  );
+});
+
+app.get("/logIn", (req, res, next) => {
+  fs.readFile(
+    __dirname + "/views/content/loginPage.html",
+    "utf8",
+    function (err, data) {
+      res.end(data);
+    }
+  );
+});
+
+app.get("/signup", (req, res, next) => {
+  fs.readFile(
+    __dirname + "/views/content/signupPage.html",
+    "utf8",
+    function (err, data) {
+      res.end(data);
+    }
+  );
+});
+
+app.get("/enterAccount", (req, res, next) => {
+  fs.readFile(
+    __dirname + "/views/content/enterAccount.html",
+    "utf8",
+    function (err, data) {
+      res.end(data);
+    }
+  );
+});
+
+app.get("/signupComplete", (req, res, next) => {
+  fs.readFile(
+    __dirname + "/views/content/signupComplete.html",
+    "utf8",
+    function (err, data) {
+      res.end(data);
+    }
+  );
+});
+
+const userList = [
+  { id: "test1", pw: "1", name: "강" },
+  { id: "test2", pw: "1", name: "김" },
+  { id: "test3", pw: "1", name: "소" },
+];
+
+app.post("/login", (req, res, next) => {
+  let rp = req.body;
+  let userInfo = undefined;
+  userList.forEach((v) => {
+    if (v.id == rp.userId && v.pw == rp.userPw) {
+      userInfo = v;
+      return;
+    }
+  });
+  console.log(rp);
+  if (userInfo) {
+    res.redirect("/main");
+    return;
+  }
+  res.send('<script>alert("실패"); location.href="/login";</script>');
+});
+
+app.post("/login2", (req, res, next) => {
+  let rp = req.body;
+  let userInfo = undefined;
+  userList.forEach((v) => {
+    if (v.id == rp.id && v.pw == rp.pw) {
+      userInfo = v;
+      return;
+    }
+  });
+  console.log(rp);
+  res.json(userInfo);
 });
--- 공길이 웹페이지.ver/public/js/login.js
+++ 공길이 웹페이지.ver/public/js/login.js
@@ -1,22 +1,78 @@
 var signInButton = document.querySelector(".loginButton");
-var errorMsg = document.querySelector(".error-msg");
+var errorMsg_null = document.querySelector(".null-account");
+var errorMsg_wrong = document.querySelector(".wrong-account");
 
+var loginForm = document.getElementById("login-wrapper");
+loginForm.onsubmit = signIn;
 signInButton.addEventListener("click", signIn);
 
+// keyboard event (엔터 눌렀을때)
+// $("form input").keydown(function (e) {
+//   if (e.keyCode == 13) {
+//     e.preventDefault();
+//     checkInput();
+//     return false;
+//   }
+// });
+
+// 입력관련 error 표시
+function checkInput() {
+  var userId = $("#userId").val();
+  var userPw = $("#userPw").val();
+  if (!userId || !userPw) {
+    errorMsg_wrong.classList.remove("error");
+    if (errorMsg_null.classList.contains("error")) {
+      return true;
+    } else {
+      errorMsg_null.classList.toggle("error");
+    }
+  } else {
+    errorMsg_null.classList.remove("error");
+    if (errorMsg_wrong.classList.contains("error")) {
+      return true;
+    } else {
+      errorMsg_wrong.classList.toggle("error");
+    }
+  }
+  return true;
+}
+
 function signIn() {
+  var r = checkInput();
+  if (!r) return;
   var userId = document.getElementById("userId").value;
   var userPw = document.getElementById("userPw").value;
 
-  if (userId === "testid" && userPw === "testpw") {
-    console.log("로그인 성공");
-    location.href = "이동할 페이지 주소 쓰기";
-  } else if (userId !== "testid" || userPw !== "testpw") {
-    console.log("로그인 실패");
+  $.ajax({
+    url: "/login2",
+    type: "post",
+    data: { id: userId, pw: userPw },
+    success: function (res) {
+      errorMsg_wrong.classList.remove("error");
+      console.log("로그인 function");
+      if (res) location.href = "/main";
+    },
+    error: function (error) {
+      console.log("로그인 실패");
+    },
+  });
 
-    if (errorMsg.classList.contains("error")) {
-      return;
-    } else {
-      errorMsg.classList.toggle("error");
-    }
-  }
+  // // if (userId === "testid" && userPw === "testpw") {
+  // if (userId === "1" && userPw === "1") {
+  //   loginForm.method = "POST";
+  //   loginForm["accept-charset"] = "utf-8";
+  //   loginForm.autocomplete = "off";
+  //   loginForm.action = "/login";
+  //   loginForm.submit();
+  //   // location.href = "/main";
+  // } else if (userId !== "testid" || userPw !== "testpw") {
+  //   console.log("로그인 실패");
+
+  //   if (errorMsg.classList.contains("error")) {
+  //     return;
+  //   } else {
+  //     errorMsg.classList.toggle("error");
+  //   }
+  // }
+  // return false;
 }
