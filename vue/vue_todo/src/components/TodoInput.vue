e2fbe719409afc4eaba8382ffa45ba842b201479
NYchicken <mirro97@naver.com>
2021-04-12 16:57:11 +0800

    vue.js 실제 응용

--- vue/vue_todo/src/components/TodoInput.vue
+++ vue/vue_todo/src/components/TodoInput.vue
@@ -4,14 +4,24 @@
       <span class="addContainer" v-on:click="addTodo">
           <i class="addBtn fas fa-plus" aria-hidden="true"></i>
       </span>
+
+      <modal v-if="showModal" @close="showModal = false">
+          <h3 slot="header">경고</h3>
+          <span slot="footer" @click="showModal=false">
+              할일을 입력하세요
+              <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+          </span>
+      </modal>
   </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
 export default {
     data(){
         return {
-            newTodoItem:''
+            newTodoItem:'',
+            showModal: false
         }
     },
     methods: {
@@ -21,12 +31,17 @@ export default {
                 // localStorage.setItem(value, value);
                 this.$emit('addTodo', value);
                 this.clearInput();
+            }else {
+                this.showModal = !this.showModal;
             }
         },
         clearInput(){
             this.newTodoItem = '';
+        },
+    },
+    components: {
+            Modal:  Modal
         }
-    }
 }
 </script>
 
