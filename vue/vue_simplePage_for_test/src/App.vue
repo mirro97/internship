c39b9e9e02fe869075aa29ac40bfd5cf063c2428
NYchicken <mirro97@naver.com>
2021-04-13 16:58:35 +0800

    vue.js 로 간단한 페이지 테스트

--- vue/vue_simplePage_for_test/src/App.vue
+++ vue/vue_simplePage_for_test/src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="black-bg" v-if="modal_is_open === true">
     <div class="white-bg">
-      <h4 @click="modal_is_open = false">상세페이지 타이틀</h4>
+      <h4>상세페이지 타이틀</h4>
       <p>상세페이지 내용</p>
+      <button @click="modal_is_open = false">닫기</button>
     </div>
   </div>
 
@@ -15,38 +16,27 @@
     <button @click="reportNum++">허위매물 신고</button>
     <span>신고수 : {{ reportNum }}</span>
   </div> -->
-  <div class="product">
-    <h4 @click="modal_is_open = true">{{ products[0] }}</h4>
-    <p>{{ price[0] }}</p>
-    <button @click="reportNum[0]++">허위매물 신고</button>
-    <span>신고수 : {{ reportNum[0] }}</span>
-  </div>
-  <div class="product">
-    <h4 @click="modal_is_open = true">{{ products[1] }}</h4>
-    <p>{{ price[1] }}</p>
-    <button @click="reportNum[1]++">허위매물 신고</button>
-    <span>신고수 : {{ reportNum[1] }}</span>
-  </div>
-  <div class="product">
-    <h4 @click="modal_is_open = true">{{ products[2] }}</h4>
-    <p>{{ price[2] }}</p>
-    <button @click="reportNum[2]++">허위매물 신고</button>
-    <span>신고수 : {{ reportNum[2] }}</span>
+
+  <div class="product" v-for="(b, i) in importData" :key="i">
+    <img :src="b.image" alt="" />
+    <h4 class="p_title" @click="modal_is_open = true">{{ b.title }}</h4>
+    <p class="p_content">{{ b.content }}</p>
+    <p class="p_price">{{ b.price }} 원</p>
+    <span class="divide"></span>
   </div>
 </template>
 
 <script>
+import data from "./post.js";
+
 export default {
   name: "App",
   data() {
     return {
-      // 데이터 보관함 - 중요한 변수들 다 때려박으면 됨 - 실시간 자동 렌더링가능
-      // 데이터는 object 자료로 저장
+      modal_is_open: false,
       menu: ["Home", "Products", "About"],
-      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
-      price: [60, 110, 80],
       reportNum: [0, 0, 0],
-      modal_is_open: false
+      importData: data
     };
   },
   methods: {
@@ -65,10 +55,25 @@ export default {
 
 body {
   margin: 0;
+  width: 100%;
+  height: 100vh;
 }
 div {
   box-sizing: border-box;
 }
+
+h4,
+p,
+span {
+  display: block;
+  margin: 0;
+}
+
+img {
+  display: block;
+  width: 100%;
+}
+
 .black-bg {
   width: 100%;
   height: 100%;
@@ -87,11 +92,36 @@ div {
 .menu {
   background: darkslateblue;
   padding: 15px;
-  border-radius: 5px;
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
 }
 
 .menu a {
   color: #fff;
   padding: 10px;
+}
+
+.product {
+  padding: 10px 10px 0 10px;
+}
+
+.p_title {
+  margin-top: 20px;
+}
+
+.p_content {
+  margin-top: 16px;
+  padding: 0 20px;
+}
+
+.p_price {
+  margin-top: 10px;
+  text-align: right;
+}
+
+.divide {
+  margin-top: 10px;
+  border-bottom: 1px solid #ced4da;
+  width: 100%;
 }
 </style>
---
+++ vue/vue_simplePage_for_test/src/test.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="black-bg" v-if="modal_is_open === true">
+    <div class="white-bg">
+      <h4 @click="modal_is_open = false">상세페이지 타이틀</h4>
+      <p>상세페이지 내용</p>
+    </div>
+  </div>
+
+  <div class="menu">
+    <a v-for="(a, i) in menu" :key="i">{{ a }}</a>
+  </div>
+  <!-- <div class="product" v-for="(a, i) in 3" :key="i">
+    <h4>{{ products[i] }}</h4>
+    <p>{{ price[i] }}</p>
+    <button @click="reportNum++">허위매물 신고</button>
+    <span>신고수 : {{ reportNum }}</span>
+  </div> -->
+  <div class="product">
+    <h4 @click="modal_is_open = true">{{ products[0] }}</h4>
+    <p>{{ price[0] }}</p>
+    <button @click="reportNum[0]++">허위매물 신고</button>
+    <span>신고수 : {{ reportNum[0] }}</span>
+  </div>
+  <div class="product">
+    <h4 @click="modal_is_open = true">{{ products[1] }}</h4>
+    <p>{{ price[1] }}</p>
+    <button @click="reportNum[1]++">허위매물 신고</button>
+    <span>신고수 : {{ reportNum[1] }}</span>
+  </div>
+  <div class="product">
+    <h4 @click="modal_is_open = true">{{ products[2] }}</h4>
+    <p>{{ price[2] }}</p>
+    <button @click="reportNum[2]++">허위매물 신고</button>
+    <span>신고수 : {{ reportNum[2] }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      // 데이터 보관함 - 중요한 변수들 다 때려박으면 됨 - 실시간 자동 렌더링가능
+      // 데이터는 object 자료로 저장
+      menu: ["Home", "Products", "About"],
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      price: [60, 110, 80],
+      reportNum: [0, 0, 0],
+      modal_is_open: false
+    };
+  },
+  methods: {
+    increase() {
+      this.reportNum += 1;
+    }
+  },
+  components: {}
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+}
+
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  widows: 100%;
+  background: #fff;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: #fff;
+  padding: 10px;
+}
+</style>
+
+<template>
+  <div>
+    hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {};
+  },
+  methods: {},
+  components: {}
+};
+</script>
+
+<style></style>
