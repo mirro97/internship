6372dbe7f790416b9ae11b4aafdc07edcaf90755
NYchicken <mirro97@naver.com>
2021-05-11 14:47:45 +0800

    새 프로젝트 시작

---
+++ vue/vue-news/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <!-- url 에 따라 화면에 표시되는 페이지가 달라진다 -->
+    <tool-bar></tool-bar>
+    <span>news</span>
+    <span>jobs</span>
+    <span>ask</span>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import ToolBar from "./components/ToolBar";
+export default {
+  components: {
+    ToolBar
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/vue-news/src/views/AskView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    asks
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
