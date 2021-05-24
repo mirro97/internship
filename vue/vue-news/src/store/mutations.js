3e9b9d9e6824c5292996142ac6793e093ff68b48
NYchicken <mirro97@naver.com>
2021-05-24 17:25:13 +0800

    HOC 부분 제거후 Mixin을 활용한 어플 구현

--- vue/vue-news/src/store/mutations.js
+++ vue/vue-news/src/store/mutations.js
@@ -1,4 +1,13 @@
 export default {
+  SET_NEWS(state, newsData) {
+    state.news = newsData;
+  },
+  SET_JOBS(state, jobsData) {
+    state.jobs = jobsData;
+  },
+  SET_ASKS(state, asksData) {
+    state.asks = asksData;
+  },
   SET_USER(state, userData) {
     state.user = userData;
   },
---
+++ vue/vue-news/src/views/NewsView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ListItem></ListItem>
+  </div>
+</template>
+
+<script>
+import ListItem from "../components/ListItem";
+import ListMixin from "../mixins/ListMixin";
+
+export default {
+  components: {
+    ListItem
+  },
+  mixins: [ListMixin]
+};
+</script>
+
+<style scoped></style>
